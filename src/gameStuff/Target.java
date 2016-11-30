c8ca35db6e507777f3a0e35077cf1dd165a74035
Bane Sullivan <chrsulli@mines.edu>
2016-11-29 20:33:42 -0700

    made a width variable for the target as well as a boolean wasHit. Made an empty function stub for the interaction that takes a missle and will eventually update the wasHit boolean appropriately

--- src/gameStuff/Target.java
+++ src/gameStuff/Target.java
@@ -2,12 +2,14 @@ package gameStuff;
 
 public class Target {
 	private int xLoc, yLoc;
+	private boolean wasHit;
 	// TODO: deal with size of target
+	private int width = 5;
 	
 	public Target(int x, int y) {
 		xLoc = x;
 		yLoc = y;
-		
+		wasHit = false;
 	}
 	
 	public int getXLoc() {
@@ -16,5 +18,11 @@ public class Target {
 	
 	public int getYLoc() {
 		return yLoc;
+	}
+	public void interact(Missile m) {
+		// create a space around target
+		// test if in x-range
+		// test if in y-range
+		
 	}
 }
