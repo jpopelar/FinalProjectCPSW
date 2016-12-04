ffdb8015b7b4e1361b0de78f18227b15b5bdaa6b
Jared Popelar <wildsnivy1@gmail.com>
2016-12-04 14:45:30 -0700

    Pre-pull commit

--- src/gui/GameWindow.java
+++ src/gui/GameWindow.java
@@ -83,13 +83,9 @@ public class GameWindow extends JFrame{
 		gui.field.launch(80);
 		gui.field.setLauncher(2);
 		gui.field.launch(30);
-<<<<<<< HEAD
 		gui.field.incrementLevel();
 		*/
-=======
-		gui.field.incrementLevel();*/
->>>>>>> 7b1f0ed283cf4fa259814aab806829abad5e36cc
-		
+
 	}
 	
 }
